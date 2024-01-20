b0a582a4e2f510752676147dfbb07b99bb3695df
SOCIALPINE <142303892+SOCIALPINE@users.noreply.github.com>
2024-01-21 00:17:42 +0900

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-
 decount = 0
 count = 0
 ifcount = 0
-compile = "Pine.pine"
+compile = "pine.pine"
 #컴파일 할 코드 
 cocount = 0
 ccount = {}
 with open(compile, "+r") as myfile:
     code = myfile.read()
-vv = code.split()
+vv = code.split(";")
 for i in vv:
   ifcount = 0
   cocount = 0
