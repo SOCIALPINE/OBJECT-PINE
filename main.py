c06e361f7309fd49cc7fdccdd97bf612db376d8b
SOCIALPINE <142303892+SOCIALPINE@users.noreply.github.com>
2024-01-20 23:42:17 +0900

    main.py

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
-global code
-global final
-from pathlib import Path
-import os
-import glob
 
 decount = 0
 count = 0
 ifcount = 0
 cocount = 0
 ccount = {}
-try:
-  with open("pine.pine", "+r") as myfile:
+with open("pine.pine", "+r") as myfile:
     code = myfile.read()
-    print()
-except Exception as e:
-  code = "1 2 3"
-  a = os.path.dirname(os.path.realpath(__file__))
-  path1 = Path(a)
-  path3 = glob.glob(a)
-  path2 = os.listdir(path3[0])
-  for files in path2:
-    if ".pine" in files:
-      final = files
-      break
-    decount += 1
-  if a[0] == '\\':
-    with open(a + "\\" + path2[decount], "+r") as myfile:
-      code = myfile.read()
-  else:
-    with open(a + "/" + path2[decount], "+r") as myfile:
-      code = myfile.read()
 vv = code.split()
 for i in vv:
   ifcount = 0
